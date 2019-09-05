d33a6bc8d9b6d6da1b480c5538563bf25927bccc
norazahora777 <54658606+norazahora777@users.noreply.github.com>
2019-09-04 21:22:55 -0300

    creaciòn de un objeto

--- semana4/action_page.php
+++ semana4/action_page.php
@@ -2,7 +2,18 @@
 //var_dump($_GET)
 	echo "Su nombre es ".$_GET["nombre"];
 	$archivo=fopen("usuarios.txt","a");
-	fwrite($archivo, $_GET["nombre"]."\n"); 
+	
+	$miobjeto=new stdClass();
+	$miobjeto->nombre="Alfredo";
+	$miobjeto->apellido="Mercurio";
+
+	
+
+fwrite($archivo,json_encode($miobjeto));
+fclose ($archivo);
+
+
+	var_dump($miobjeto);
 		# code...
 	
 
